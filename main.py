93c545e024f12ab20ec98c5a44024f03d6c58f9b
Isidora Vizcaya <isidoravizcaya@gmail.com>
2019-08-31 20:29:23 -0400

    feat(chat): add timestamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
@@ -12,10 +13,10 @@ def sessions():
 def messageReceived(methods=['GET', 'POST']):
   print('SUCCESS! Message received')
 
-@socketio.on('event')
+@socketio.on('chat_event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
   print('received event: ' + str(json))
-  socketio.emit('response', json, callback=messageReceived)
+  socketio.emit('chat_response', json, callback=messageReceived)
 
 if __name__ == '__main__':
   socketio.run(app, debug=True)
